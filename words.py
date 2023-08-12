e42fc5aaacc71185461e6d83ba22ac113bde6c1b
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-08-12 19:46:35 +0000

    Add Add words for difficulty levels

--- words.py
+++ words.py
@@ -1,26 +1,35 @@
-word_list = ['elephant',
-             'guitar',
-             'computer',
-             'pizza',
-             'bicycle',
-             'diamond',
-             'rainbow',
-             'spider',
-             'telescope',
-             'dragon',
-             'house',
-             'book',
-             'bed',
-             'summer',
-             'street',
-             'python',
-             'school',
-             'computer',
-             'park',
-             'office',
-             'bodybuilder',
-             'children',
-             'market',
-             'store',
-             'beach',
-             'holidays']
+word_list_easy = [
+    'dog', 'cat', 'book', 'table', 'chair', 'house', 'car', 'tree', 'sun',
+    'moon', 'water', 'fire', 'bed', 'park', 'office', 'market', 'store',
+    'beach', 'love', 'small', 'zoo'
+]
+
+word_list_intermediate = [
+    'schedule',
+    'ticket',
+    'bodybuilder',
+    'doorbell',
+    'geography',
+    'avocato',
+    'kangoroo'
+    'preferential',
+    'raspberry',
+    'strawberry',
+    'pumpklin',
+    'kettle',
+    'ballon',
+    'napkin',
+    'kitten',
+    'shampoo',
+    'children',
+    'summer',
+    'street',
+    'python',
+    'school',
+]
+
+word_list_hard = [
+    'throughout', 'complimentary', 'otherwise', 'although', 'faithfully',
+    'inwards', 'entrepreneur', 'nonplussed', 'irregardless', 'xenophobia',
+    'disinterested', 'enormity', 'obsequious', 'subtle', 'debris'
+]
