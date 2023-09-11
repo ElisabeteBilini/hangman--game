ad8e332723288021de2a60c81979c3743bc46f1b
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-09-11 18:55:22 +0000

    Fix long lines error, Fix empty input validation and Add comments in code

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 '''Template exchange, originally written by the link:
 https://elisabetebilini.github.io/hangman-game/
 '''
+# Libraries and modules
 import random
 import re
 from art import body_pieces, hang, positive
@@ -8,6 +9,7 @@ from words import word_list_easy, word_list_intermediate, word_list_hard
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Google Sheets configuration
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -22,17 +24,19 @@ SHEET = GSPREAD_PLAYER.open('Hangman--game-project')
 game_difficulty = ""
 
 
+# Function to update scores in Google Sheets
 def update_scores(player_name, score, difficulty):
     worksheet = SHEET.worksheet("Scores")
     data = [player_name, score, difficulty]
     worksheet.append_row(data)
 
 
+# Function to display player scores from Google Sheets
 def display_scores():
     worksheet = SHEET.worksheet("Scores")
     scores = worksheet.get_all_values()[1:]
     scores.sort(key=lambda x: int(x[1]), reverse=True)
-    print("\nPlayer Scores:")
+    print("\n--------Player Scores:---------")
     print("\nPlayer Name | Score | Difficulty")
     print("-" * 30)
     for row in scores:
@@ -46,6 +50,7 @@ print('Welcome to')
 print(hang)
 
 
+# Function to display game instructions
 def display_instructions():
     print("\n----------------Instructions for Hangman:-----------------\n")
     print("→ The player can play alone or against another player.")
@@ -59,11 +64,12 @@ def display_instructions():
         pass
 
 
+# Variables
 select_word = ""
 player_01 = ""
 player_02 = ""
 
-
+# Main game
 while True:
     print("\n1 - Instructions")
     print("2 - Player Scores")
@@ -84,16 +90,25 @@ while True:
         display_scores()
     elif choice == '3':
         while True:
-            choice = input("Choose the game mode:\n1 - Single Player\n2 - Two Players\n")  # noqa
+            choice = input("\nChoose the game mode:\n"
+                           "1 - Single Player\n"
+                           "2 - Two Players\n")
+
             if choice == '1' or choice == '2':
                 break
             else:
-                print("Invalid choice. Please enter '1' for Single Player or '2' for Two Players.")  # noqa
+                print("\nInvalid choice. Please enter:\n"
+                      "1 - Single Player\n"
+                      "2 - Two Players\n")
 
         if choice == '1':
             while True:
-                game_difficulty = input("Choose the difficulty level:\n1 - Easy\n2 - Intermediate\n3 - Hard\n")  # noqa
-                if game_difficulty == '1' or game_difficulty == '2' or game_difficulty == '3':  # noqa
+                game_difficulty = input("\nChoose the difficulty level:\n"
+                                        "1 - Easy\n"
+                                        "2 - Intermediate\n"
+                                        "3 - Hard\n")
+
+                if game_difficulty in ('1', '2', '3'):
                     break
                 else:
                     print("Invalid option. Please choose 1, 2, or 3.")
@@ -105,34 +120,37 @@ while True:
             else:
                 select_word = random.choice(word_list_hard).upper()
 
-            BOARD = []
-            BLANKS = '_'
-            CHANCES = 7
-            GAME_OVER = False
-            used_letters = []
+            BOARD = []  # Initialize game board
+            BLANKS = '_'  # Blank spaces in the word
+            CHANCES = 7  # Number of chances
+            GAME_OVER = False  # Indicate if the game is over
+            used_letters = []  # List to store used letters
 
             for letter in range(len(select_word)):
                 BOARD += BLANKS
 
-            # get player's name
+            # Main game - single player
             while True:
                 player = input("Player, enter your name: \n").upper()
                 if re.match(r'^[A-Z]{3,}$', player):
                     break
                 else:
-                    print("Invalid name. Please enter a name with at least 3 letters and containing only letters.")  # noqa
+                    print("Invalid name.\n"
+                          "Enter a name longer than 3 letters (only letters)")
 
             print("Number of letters in the secret word: ", len(select_word))
 
             atual_player = player
 
-            # identifying player
             while not GAME_OVER:
                 print('Secret Word: ' + ''.join(BOARD))
                 print("Used Letters:", ', '.join(used_letters))
 
                 while True:
                     letter_player = input("Type one letter: \n").upper()
+                    if not letter_player:
+                        print("Please enter a letter.")
+                        continue
                     if len(letter_player) == 1 and letter_player.isalpha():
                         if re.match(r'^[A-Z]{3,}$', letter_player):
                             break
@@ -183,8 +201,12 @@ while True:
 
         elif choice == '2':
             while True:
-                game_difficulty = input("Choose the difficulty level:\n1 - Easy\n2 - Intermediate\n3 - Hard\n")  # noqa
-                if game_difficulty == '1' or game_difficulty == '2' or game_difficulty == '3':  # noqa
+                game_difficulty = input("\nChoose the difficulty level:\n"
+                                        "1 - Easy\n"
+                                        "2 - Intermediate\n"
+                                        "3 - Hard\n")
+
+                if game_difficulty in ('1', '2', '3'):
                     break
                 else:
                     print("Invalid option. Please choose 1, 2, or 3.")
@@ -211,19 +233,22 @@ while True:
                 if re.match(r'^[A-Z]{3,}$', player_01):
                     break
                 else:
-                    print("Invalid name. Please enter a name with at least 3 letters and containing only letters.")  # noqa
+                    print("Invalid name.\n"
+                          "Enter a name longer than 3 letters (only letters)")
 
             while True:
                 player_02 = input("Player 2, enter your name: \n").upper()
                 if re.match(r'^[A-Z]{3,}$', player_02):
                     break
                 else:
-                    print("Invalid name. Please enter a name with at least 3 letters and containing only letters.")  # noqa
+                    print("Invalid name.\n"
+                          "Enter a name longer than 3 letters (only letters)")
 
             print("Number of letters in the secret word: ", len(select_word))
 
             atual_player = player_01
 
+            # Main game - two players
             while not GAME_OVER:
                 print('Secret Word: ' + ''.join(BOARD))
                 print(f"{atual_player}'s turn.")
@@ -231,6 +256,9 @@ while True:
 
                 while True:
                     letter_player = input("Type one letter: \n").upper()
+                    if not letter_player:
+                        print("Please enter a letter.")
+                        continue
                     if len(letter_player) == 1 and letter_player.isalpha():
                         if letter_player in used_letters:
                             print("You've already used this letter.")
@@ -284,9 +312,10 @@ while True:
                     if 0 < CHANCES < len(body_pieces):
                         print(body_pieces[CHANCES])
 
+    # Choice and confirmation to quit
     elif choice == '4':
         while True:
-            confirm_exit = input("Are you sure you want to quit? (y/n): ")
+            confirm_exit = input("\nAre you sure you want to quit? (y/n): ")
             if confirm_exit.lower() == 'y':
                 print("\nThanks for playing Hangman!")
                 print(positive)
@@ -295,7 +324,8 @@ while True:
             elif confirm_exit.lower() == 'n':
                 break
             else:
-                print("Invalid input. Please enter 'y' to quit or 'n' to cancel.")  # noqa
+                print("Invalid input.\n"
+                      "Please enter 'y' to quit or 'n' to cancel.")
 
         if confirm_exit.lower() == 'y':
             break
